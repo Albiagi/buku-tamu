da9fce22b6433897d673ea1244d06ab525b5fcc3
Aru Hanazawa <williwilliam4@gmail.com>
2024-03-01 11:27:52 +0700

    edit by Alby

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="col-sm-2 min-vh-100" style="background-color:rgb(45, 85, 172)">
+    <header class="py-2 px-3 text-center border-bottom">
+        <span class="fs-4 fw-bolder text-white">Dashboard</span>
+    </header>
+    <div class="wrapper mt-2 d-flex flex-column justify-content-center" style="min-height: 40%">
+        <div class="mb-2 button-menu">
+            <a href="" class="px-3 text-white text-decoration-none"><i class="bi bi-people"></i><span> Guest List</span></a>
+        </div>
+        <div class="mb-2 button-menu">
+            <a href="" class="px-3 text-white text-decoration-none"><i class="bi bi-person-circle"></i></i><span> Master Data</span></a>
+        </div>
+    </div>
+    <div class="d-flex align-items-end" style="min-height: 50%">
+        <div class="button-menu">
+            <a href="" class="px-3 text-white text-decoration-none"><i class="bi bi-box-arrow-left"></i></i><span> Log Out</span></a>
+        </div>
+    </div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,24 +3,7 @@
 @section('konten')
 <div class="container-fluid">
     <div class="row">
-        <div class="col-sm-2 min-vh-100" style="background-color:rgb(45, 85, 172)">
-            <header class="py-2 px-3 text-center border-bottom">
-                <span class="fs-4 fw-bolder text-white">Dashboard</span>
-            </header>
-            <div class="wrapper mt-2 d-flex flex-column justify-content-center" style="min-height: 40%">
-                <div class="mb-2 button-menu">
-                    <a href="" class="px-3 text-white text-decoration-none"><i class="bi bi-people"></i><span> Guest List</span></a>
-                </div>
-                <div class="mb-2 button-menu">
-                    <a href="" class="px-3 text-white text-decoration-none"><i class="bi bi-person-circle"></i></i><span> Master Data</span></a>
-                </div>
-            </div>
-            <div class="d-flex align-items-end" style="min-height: 50%">
-                <div class="button-menu">
-                    <a href="" class="px-3 text-white text-decoration-none"><i class="bi bi-box-arrow-left"></i></i><span> Log Out</span></a>
-                </div>
-            </div>
-        </div>
+        @include('components.sidebar')
         <div class="col bg-white min-vh-100">
             <div class="mt-2 border-bottom">
                 @include('components.navbar')
@@ -56,8 +39,5 @@
             </div>
         </div>
     </div>
-
 </div>    
-
-
 @endsection
